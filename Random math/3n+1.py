27fbf12d52faf2e2ea3acfbe92edd9727c290005
Hoaz <148769157+CookWang1906@users.noreply.github.com>
2024-02-06 20:47:51 +0700

    Update 3n+1.py

--- Random math/3n+1.py
+++ Random math/3n+1.py
@@ -4,9 +4,9 @@ n = int(input('Please input your number here: '))
 while n < n*n:
     if n % 2==0:
         n = n/2
-        print('Divi', n)
+        print('Down', n)
     else:
         n = n*3 + 1
-        print('Muti', n)
+        print('Up', n)
     if n == 1:
         break
